b9ea6b4019443a321fd2a6cc1e092ec062951d7f
Liam-Kinney <Kiliam@umich.edu>
2025-04-09 12:53:05 -0400

    Liam worked on creating graph comparing genres and playcounts, completed it

--- Liam-Code.py
+++ Liam-Code.py
@@ -2,6 +2,7 @@ import requests
 import sqlite3
 import json
 import random
+import matplotlib.pyplot as plt
 import re
 
 API_KEY = "08d3b443588c0efe5d7803f6c8b91630"
@@ -98,3 +99,25 @@ for artist, track in track_list:
 
 conn.commit()
 conn.close()
+
+conn = sqlite3.connect("tracks.db")
+cur = conn.cursor()
+
+cur.execute('''
+    SELECT genre, AVG(playcount) FROM tracks WHERE genre != 'unknown'
+    GROUP BY genre HAVING COUNT(*) >= 5 ORDER BY AVG(playcount) DESC
+''')
+
+results = cur.fetchall()
+conn.close()
+
+genres = [row[0].title() for row in results]
+avg_playcounts = [row[1] for row in results]
+
+plt.figure(figsize=(10, 6))
+plt.scatter(avg_playcounts, genres, s = 100)
+plt.ylabel("Genre")
+plt.xlabel("Average Playcount (in millions)")
+plt.title("Average Playcounts by Genre")
+plt.grid(axis = 'x', linestyle = '--', alpha = 0.7)
+plt.show()
---
+++ genre_vs_playcount_plot.py
@@ -0,0 +1,24 @@
+import sqlite3
+import matplotlib.pyplot as plt
+
+conn = sqlite3.connect("tracks.db")
+cur = conn.cursor()
+
+cur.execute('''
+    SELECT genre, AVE(playcount) FROM tracks WHERE genre != 'unknown'
+    GROUP BY genre ORDER BY AVG(playcount) DESC
+''')
+
+results = cur.fetchall()
+conn.close()
+
+genres = [row[0].title() for row in results]
+avg_playcounts = [row[1] for row in results]
+
+plt.figure(figsize=(10, 6))
+plt.bar(genres, avg_playcounts)
+plt.xlabel("Genre")
+plt.ylabel("Average Playcount")
+plt.title("Average Playcounts by Genre")
+plt.xticks(rotation=90, ha = 'right')
+plt.show()
